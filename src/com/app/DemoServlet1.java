140429892cf602cfb66afadf9587d3915c4b267b
Naresh pc <Naresh pc@192.168.0.20>
2019-02-12 12:28:58 +0530

    demo class is added.....

--- src/com/app/DemoServlet1.java
+++ src/com/app/DemoServlet1.java
@@ -8,41 +8,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class DemoServlet1
- */
+ 
 @WebServlet("/DemoServlet1")
 public class DemoServlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * Default constructor. 
-     */
-    public DemoServlet1() {
+ public DemoServlet1() {
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
+ 
 	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+ 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-	}
-
+	}@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+		 
+	} 
 }
---
+++ src/com/app/test/DemoClass.java
@@ -0,0 +1,12 @@
+package com.app.test;
+
+public class DemoClass { 
+public static void main(String[] args) {
+	 
+	System.out.println("Naresh panda");
+	
+	
+}
+
+
+}
